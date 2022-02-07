5b5c4d0128b012e54e04177bd46cf3c0bfd99b7e
Edwin Uber <edwin.uber@gmail.com>
2022-02-07 10:27:55 -0600

    Initial Commit

---
+++ RestaurantsUber/RestaurantsUberApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantsUberApp.swift
+//  RestaurantsUber
+//
+//  Created by Edwin Uber on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantsUberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
